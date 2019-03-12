9308da96259fd6b0ed6ea2da4a6f6630e1f590d9
Manuel Mujica <manuel.expat@gmail.com>
2019-03-12 13:02:37 -0300

    Pass expanded path to the default resolver

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
+var defaultResolver = require("jest-resolve/build/defaultResolver").default;
 
-// Given a module identifier with a trailing slash, 
-// it returns the last identifier segment 
+// Given a module identifier with a trailing slash,
+// it returns the last identifier segment
 // getTrailingSegment('./foo/bar/') -> "bar"
 function getTrailingSegment(path) {
-  var parts = path.split('/');
-  return lastIdSegment = parts[parts.length - 2];
+  var parts = path.split("/");
+  return parts[parts.length - 2];
 }
 
 // trailingSlash : (string, resolverOptions) -> string
 module.exports = function trailingSlash(path, resolverOptions) {
-  return path.endsWith('/')
-    ? `${path}${getTrailingSegment(path)}`
-    : path;
+  return defaultResolver(
+    path.endsWith("/") ? `${path}${getTrailingSegment(path)}` : path,
+    resolverOptions
+  );
 };
